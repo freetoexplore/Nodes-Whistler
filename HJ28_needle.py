363309d37d1b77952bb0b14fd76c80866239fed4
freetoexplore <116438028+freetoexplore@users.noreply.github.com>
2024-02-18 19:41:04 +0800

    Update HJ28_needle.py

--- HJ28_needle.py
+++ HJ28_needle.py
@@ -1,10 +1,1 @@
-haystack = "sadbutsad"
-needle = "sad"
-window = len(needle)
-res = -1
-for i in range(len(haystack) - window):
-    if haystack[i:i+window] == needle:
-        res = i
-        break
-    
-print(res)
+\wrong file
